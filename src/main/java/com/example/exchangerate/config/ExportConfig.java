226bbbd8cd66fd7a0a73549d883ba7f21ba0cbd2
shivamk01here <sk8612181@gmail.com>
2026-06-19 12:35:41 +0530

    feat: add JSON export settings to ExportConfig

--- src/main/java/com/example/exchangerate/config/ExportConfig.java
+++ src/main/java/com/example/exchangerate/config/ExportConfig.java
@@ -11,4 +11,12 @@ public class ExportConfig {
 
     private int maxExportSize = 10000;
     private String filename = "conversion-history";
+    private JsonSettings json = new JsonSettings();
+
+    @Data
+    public static class JsonSettings {
+        private boolean prettyPrint = false;
+        private String dateFormat = "iso";
+        private boolean includeNullFields = false;
+    }
 }
